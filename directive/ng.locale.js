49dde99f270c5c3c7aa490a4a8515cf1706746fd
Alexander <ketov.alex.v@yandex.kz>
2017-04-11 19:54:07 +0600

    - changed structure.

---
+++ directive/ng.locale.js
@@ -0,0 +1,17 @@
+angular.module('ng.locale', [])
+    .directive('ngLocale', ['ngLocaleService', ngLocale]);
+
+ngLocale.$inject = ['ngLocaleService'];
+
+function ngLocale(ngLocaleService) {
+    
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            
+            ngLocaleService.$get(attrs.ngLocale).then(function (response) {
+                element.html(response);
+            });
+        }
+    };
+}
